cfa2267cf09452042929f3d4926fe6dcd647acb0
abu saad <abusaad688@gmail.com>
2024-11-29 18:55:52 +0530

    add comment,like,playlist and tweet models

---
+++ src/models/comment.models.js
@@ -0,0 +1,34 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const commentSchema = new Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    video: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true } // Schema options
+);
+
+// Add pagination plugin
+commentSchema.plugin(mongooseAggregatePaginate);
+
+// Create the model
+const Comment = mongoose.model("Comment", commentSchema);
+
+export default Comment;
